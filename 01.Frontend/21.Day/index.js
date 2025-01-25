478290bebd30ea8f44d8881e9d858ec50e9870a7
Vignesh061 <vigneshvicky3611@gmail.com>
2025-01-25 22:59:58 +0530

    Implemented Random Number Generator

--- 01.Frontend/21.Day/index.js
+++ 01.Frontend/21.Day/index.js
@@ -5,10 +5,12 @@ let rm=0;
 
 mybutton.onclick=function()
 {
-    min=mininput.value;
-    min=Number(min);
-    max=maxinput.value;
-    max=Number(max)
-rm=Math.floor(Math.random()*(max-min))+min;
-mylabel.textContent=rm;
+
+       min=mininput.value;
+        min=Number(min);
+        max=maxinput.value;
+        max=Number(max);
+    
+        rm=Math.floor(Math.random()*(max-min))+min;
+        mylabel.textContent=`Random Number from ${min} to ${max}:${rm}`;
 }

1a4c999fd4fc46dcd45d56c54cdbbb5dfc50cdab
Vignesh061 <vigneshvicky66005@gmail.com>
2025-01-25 22:27:35 +0530

    Implemented Random Number Genenrator

--- 01.Frontend/21.Day/index.js
+++ 01.Frontend/21.Day/index.js
@@ -1,11 +1,14 @@
+const mininput=document.getElementById("mininput");
+const maxinput=document.getElementById("maxinput");
 const mybutton=document.getElementById("mybutton");
-const mylabel=document.getElementById("mylabel");
-const min=10;
-const max= 16;
-let rm;
+let rm=0;
 
 mybutton.onclick=function()
 {
+    min=mininput.value;
+    min=Number(min);
+    max=maxinput.value;
+    max=Number(max)
 rm=Math.floor(Math.random()*(max-min))+min;
 mylabel.textContent=rm;
 }

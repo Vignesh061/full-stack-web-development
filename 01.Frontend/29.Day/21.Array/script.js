4cfbad3f2392f814f7c8165d34363c0ffcede8b5
Vignesh061 <vigneshvicky66005@gmail.com>
2025-02-03 21:48:49 +0530

    Created Array in js

---
+++ 01.Frontend/29.Day/21.Array/script.js
@@ -0,0 +1,5 @@
+//Array
+//  an array is a special type of object used to store multiple values in a single variable. 
+// Arrays allow you to group related data and access them using an index.
+
+let fruits = ["Apple", "Banana", "Cherry"];

b7f11e18d41709dd7d708f3bb882baf43f71057b
Vignesh061 <vigneshvicky3611@gmail.com>
2026-01-13 21:58:53 +0530

    Dynamic Url

--- 02.Backend/Flask/01.App/users/routes.py
+++ 02.Backend/Flask/01.App/users/routes.py
@@ -6,4 +6,6 @@ users_bp=Blueprint("users",__name__)
 def user():
     return "This is user page"
 
-    
+@users_bp.route('/users/<user_id>') ## Dynamic Url
+def user_id(user_id):
+    return f"User id is {user_id}"
